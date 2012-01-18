544cb023fcab758b671250a2a787add4664cf001
jrusso <jrusso@CHS-ROBO-03.carmelunified.org>
2012-01-18 15:30:05 -0800

    Added placeholder subsystem classes

---
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * 
+ * Handles drive motors, transmission, gyros for autonomous balancing, and
+ * joystick input
+ *
+ * @author jrusso
+ */
+public class DriveTrain extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
---
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/subsystems/Turret.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * 
+ * Handles network tables, ball shooter activation, 
+ * turret rotation motors (pitch and yaw), potential shooter gyro, and
+ * joystick inputs
+ *
+ * @author jrusso
+ */
+public class Turret extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
