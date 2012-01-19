dc8a6fa29fb57527197f07d3589f9d26b286ed69
plevinson <plevinson@CHS-ROBO-05.carmelunified.org>
2012-01-18 17:46:17 -0800

    attempting to figure out how to create the DriveTrain

--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -5,6 +5,10 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  * 
@@ -14,11 +18,39 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @author jrusso
  */
 public class DriveTrain extends Subsystem {
+    
+    private RobotDrive drive;
+    private Joystick joystick1;
+    private Transmission transmission1;
+    private DigitalInput sensor1;
+    private DigitalInput sensor2;
+    private Jaguar lfJag;
+    private Jaguar rtJag;
+    private Jaguar lfFrontJag;
+    private Jaguar rtFrontJag;
+    
+    
+    Jaguar motor; 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+    
+    public DriveTrain(){
+        lfJag = new Jaguar(1);
+        rtJag = new Jaguar(2);
+        lfFrontJag = new Jaguar (3);
+        rtFrontJag = new Jaguar (4);
+        joystick1 = new Joystick(1);
+        sensor1 = new DigitalInput(1);
+        sensor2 = new DigitalInput (2);
+    }
+      
     public void initDefaultCommand() {
+        
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void move(double speed) {
+        motor.set(speed);
     }
 }
