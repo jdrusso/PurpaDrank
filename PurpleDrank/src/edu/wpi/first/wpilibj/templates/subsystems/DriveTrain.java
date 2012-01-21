48073eec50ca86a2c36761a4464aa1dc7c808bfd
plevinson <plevinson@CHS-ROBO-05.carmelunified.org>
2012-01-20 17:14:27 -0800

    Outline of the drive train we will need. Input values are all hypothetical now.

--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Gyro;
 
 /**
  * 
@@ -21,36 +23,46 @@ public class DriveTrain extends Subsystem {
     
     private RobotDrive drive;
     private Joystick joystick1;
-    private Transmission transmission1;
+    private Joystick joystick2;
+    private Encoder transmission1;
     private DigitalInput sensor1;
     private DigitalInput sensor2;
     private Jaguar lfJag;
     private Jaguar rtJag;
     private Jaguar lfFrontJag;
     private Jaguar rtFrontJag;
-    
-    
-    Jaguar motor; 
+    private double motorSpeeds;
+    private Gyro gyro1;
+    Jaguar motor1;
+    Jaguar motor2;
+ 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     
     public DriveTrain(){
+        drive = new RobotDrive(lfJag, rtJag, lfFrontJag, rtFrontJag);
         lfJag = new Jaguar(1);
         rtJag = new Jaguar(2);
         lfFrontJag = new Jaguar (3);
         rtFrontJag = new Jaguar (4);
         joystick1 = new Joystick(1);
+        joystick2 = new Joystick(2);
         sensor1 = new DigitalInput(1);
         sensor2 = new DigitalInput (2);
+        gyro1= new Gyro(1);
     }
       
-    public void initDefaultCommand() {
-        
+    public void initDefaultCommand() {  
+        drive.arcadeDrive(joystick1);                                           //depeding on which driving method we are using.
+        drive.tankDrive(joystick1, joystick2);
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
     public void move(double speed) {
-        motor.set(speed);
+        motor1.set(speed);
+        motor2.set(-speed);
     }
+    
+
 }
