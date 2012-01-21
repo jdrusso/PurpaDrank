0237a0a29e4944b0b02221065248074dc946b87d
plevinson <plevinson@CHS-ROBO-05.carmelunified.org>
2012-01-20 17:53:08 -0800

    command template for Shooting

---
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/Shooting.java
@@ -0,0 +1,41 @@
+
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.subsystems.Turret;
+
+/**
+ *This command will run when a button telling the robot to shoot is pressed. 
+ * It will launch the 3 balls at the basketball hoop. 
+ * @author plevinson
+ */
+
+public class Shooting extends CommandBase {
+    
+    public Shooting() {
+            requires(Turret);
+                         
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
