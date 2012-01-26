7e2b3b434e1022064901aa0e6cd80b7f0e20b483
kgabbott <kgabbott@att.net>
2012-01-25 18:16:26 -0800

    1/25/12

--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/OI.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -1,65 +1,43 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST Team 2035, 2012. All Rights Reserved.                  */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package edu.wpi.first.wpilibj.templates;
-
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.templates.commands.ArmDown;
-import edu.wpi.first.wpilibj.templates.commands.ArmReset;
-import edu.wpi.first.wpilibj.templates.commands.AutoBalancing;
-import edu.wpi.first.wpilibj.templates.commands.ManualBalancing;
-import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.templates.commands.*;
 
-/**
- * This is the Operator Interface class of the Robot code.
- * 
- * Code relating to joysticks, buttons, etc and their interactions
- * with the subsystems and commands are here.
- */
 public class OI {
     // Process operator interface input here.
-    private Joystick Joystick1;
-    private Joystick Joystick2;
+    private static Joystick joystick1;
+    private static Joystick joystick2;
     private Button Balance;
-    private Button RampControl;
     private static Button endAutoBalance;
     private DriveTrain DriveTrain;
     
-    /**
-     * Constructor for the OI class.  This is where input events are 
-     * registered (connected) to the commands. 
-     */
     public OI() {
         DriveTrain = PurpleDrank.getDriveTrain();
-        Joystick1 = DriveTrain.getJoystick1();
+        joystick1 = new Joystick(1);
         //Joystick2 = DriveTrain.getJoystick2();
         //AutoBalancing h = new AutoBalancing(.25/40, 0, .1/20); 
         //h.start();
-        endAutoBalance = new JoystickButton (Joystick1, 3);
-        Balance = new JoystickButton( Joystick1, 2);
+        endAutoBalance = new JoystickButton (joystick1, 3);
+        Balance = new JoystickButton( joystick1, 2);
         Balance.whileHeld(new ManualBalancing());
-        Balance.whenReleased(new AutoBalancing(RobotMap.Kp, RobotMap.Ki, RobotMap.Kd));
-        RampControl = new JoystickButton(Joystick1, 4);
-        RampControl.whenPressed(new ArmDown());
-        RampControl.doublePressed(new ArmReset());
+        Balance.whenReleased(new AutoBalancing(.25/40, 0, .1/20));
     
     }
     
-    /** getButton3()
-     * This can be used as getButton3().get() to return the button
-     * state.
-     * 
-     * @return the button 3 on Joystick 1 object.
-     * 
-     */
     public static Button getButton3(){
         return endAutoBalance;
     }
+    
+    public static Joystick getJoystick1(){
+        return joystick1;
+    }
+    
+    public static Joystick getJoystick2(){
+        return joystick2;
+    }
+    
 }
 
--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/ManualBalancing.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/ManualBalancing.java
@@ -1,75 +1,52 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST Team 2035, 2012. All Rights Reserved.                  */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.wpi.first.wpilibj.templates.commands;
-
 import edu.team2035.meta.MetaGyro;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.templates.subsystems.*;
 import edu.wpi.first.wpilibj.templates.PurpleDrank;
-import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
 
 /**
- * Manual Balancing command for the Robot
+ *
  * @author abbottk
- * 
- * Handles operator control balancing of the robot using a gyroscope.
  */
 public class ManualBalancing extends CommandBase{
 
     private DriveTrain DriveTrain;
     private MetaGyro gyro1;
     
-    /**
-     * 
-     */
-    public ManualBalancing() {
-       super();
+   public ManualBalancing() {
+       super("ManualBalancing");
        DriveTrain = PurpleDrank.getDriveTrain(); 
        gyro1 = DriveTrain.getGyro1();
        requires(DriveTrain);
     }
 
     // Called just before this Command runs the first time
-    /**
-     * 
-     */
     protected void initialize() {
         gyro1.reset();
+        DriveTrain.getCommandLog().setCommand(this.getName());
     }
 
     // Called repeatedly when this Command is scheduled to run
-    /**
-     * 
-     */
     protected void execute() {
         DriveTrain.move(1);
         
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    /**
-     * 
-     * @return 
-     */
     protected boolean isFinished() {
         return false;
     }
 
     // Called once after isFinished returns true
-    /**
-     * 
-     */
     protected void end() {
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    /**
-     * 
-     */
     protected void interrupted() {
     }
 }
