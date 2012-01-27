7b5cd48eb9e6b3558684623eccdeb89c17a5f6be
Toastertaster <travis.axtell@gmail.com>
2012-01-27 04:42:11 +0000

    Updating the comment banner on .java files.

--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/OI.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -1,11 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST Team 2035, 2012. All Rights Reserved.                  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package edu.wpi.first.wpilibj.templates;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.templates.commands.*;
 
+/**
+ * 
+ * @author Team 2035 Programmers
+ */
 public class OI {
     // Process operator interface input here.
     private static Joystick joystick1;
--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/DefaultDriveTrain.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/DefaultDriveTrain.java
@@ -1,12 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST Team 2035, 2012. All Rights Reserved.                  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package edu.wpi.first.wpilibj.templates.commands;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.templates.PurpleDrank;
 import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
+
 /**
  *
  * @author abbottk
