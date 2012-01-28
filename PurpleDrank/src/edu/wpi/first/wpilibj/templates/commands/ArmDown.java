a2fc0ad4ddc44138c37b6d237a960cf81fe3db49
plevinson <plevinson@CHS-ROBO-05.carmelunified.org>
2012-01-27 17:27:49 -0800

    adding comments :D

--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/ArmDown.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/ArmDown.java
@@ -11,36 +11,50 @@ import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.templates.subsystems.RampController;
 
 /**
- *
+ *This class controls the robot arm, designed to push the ramp down.
+ * 
  * @author plevinson
  */
 public class ArmDown extends CommandBase {
     private RampController Ramp;
     
+    /*
+     * Constructor to make the arm push the ramp down.
+     */
     public ArmDown() {
         super("Arm Down");
         requires(Ramp);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
-
-    // Called just before this Command runs the first time
+    
+    /*
+     * This is called right before the command is run.
+     * It sets the motor direction to forward.
+     */
     protected void initialize() {
         RampController.getRelay1().setDirection(Relay.Direction.kForward); 
 
     }
 
-    // Called repeatedly when this Command is scheduled to run
+    /*
+     * This is the actual action of the arm pushing the ramp down. 
+     * It will run until told to stop by button4 being double pressed (I think).We haven't decided yet. 
+     */
     protected void execute() {
         Ramp.push();
     }
 
-    // Make this return true when this Command no longer needs to run execute()
+    /*
+     *Once button4 is double pressed, this will change to false and stop the execute method. 
+     * 
+     * @return true to stop command, false to countinue execute()
+     */
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
+    //Called once after isFinished returns true
     protected void end() {
     }
 
