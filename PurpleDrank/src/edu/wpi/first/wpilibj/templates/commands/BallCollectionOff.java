886d9d12f370653c21227ddcca2c556271f21ad9
plevinson <plevinson@CHS-ROBO-05.carmelunified.org>
2012-02-04 12:05:53 -0800

    changed ballcollection commands so they do things...i hope

--- PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/BallCollectionOff.java
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/BallCollectionOff.java
@@ -3,30 +3,38 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.templates.subsystems.BallCollector;
 
 
 /**
+ * The Ball Collector should be running continously but this command is just
+ * here in case we need to get it to stop. 
  *
- * @author Administrator
+ * @author plevinson
  */
 public class BallCollectionOff extends CommandBase {
     private BallCollector ballCollection;
     
     public BallCollectionOff() {
-        super ("Ball Colect Off");
+        super ("Ball Collect Off");
         requires (ballCollection);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
-
-    // Called just before this Command runs the first time
+    
+    /**
+     * Called just before this Command runs the first time
+     * turns the motor off
+     */
     protected void initialize() {
+        BallCollector.getRelay().setDirection(Relay.Direction.kBoth);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        ballCollection.ballCollectorOff();
+        ballCollection.collect();  
     }
 
     // Make this return true when this Command no longer needs to run execute()
