944fe16864dc5a1139445faf4db8a0c8772565aa
jrusso <jrusso@CHS-ROBO-03.carmelunified.org>
2012-02-19 16:53:15 -0800

    Adding manual control of the turret axis.

---
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/HorizontalTurretRotationManual.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.templates.PurpleDrank;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+import edu.wpi.first.wpilibj.templates.subsystems.HorizontalTurretAxis;
+
+/**
+ *
+ * @author jrusso
+ */
+public class HorizontalTurretRotationManual extends CommandBase {
+    public int direction;
+    //public Relay HorTurretSpike;
+    public HorizontalTurretAxis ht;
+    
+    public HorizontalTurretRotationManual(int directionIn) {
+        ht = PurpleDrank.getHorizontalTurretAxis();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        direction = directionIn;
+        //HorTurretSpike = RobotMap.HorTurretMotorSpike;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        ht.rotate(direction);
+//        if (direction > 0)
+//            HorTurretSpike.set(Relay.Value.kForward);
+//        else
+//            HorTurretSpike.set(Relay.Value.kReverse);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+//        HorTurretSpike.set(Relay.Value.kOff);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+//        HorTurretSpike.set(Relay.Value.kOff);
+    }
+}
