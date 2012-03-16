c83781abde8f64538f2aa392037181b49ae5ae07
jesusrambo <thejesusrambo@gmail.com>
2012-03-15 18:41:24 -0700

    Autonomous

---
+++ PurpleDrank/src/edu/wpi/first/wpilibj/templates/commands/WinAutonomous.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.templates.PurpleDrank;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.templates.commands.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class WinAutonomous extends CommandGroup {
+    
+    public WinAutonomous() {
+    }
+}
